a418c7a65b4503775e09c5d5edeb2bd387b0c135
Trevor Benyack <trevorbenyack@gmail.com>
2021-06-08 10:41:56 -0400

    Initial commit

---
+++ src/main/java/org/perscholas/caseStudy/CaseStudyBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.caseStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyBackEndApplication.class, args);
+    }
+
+}
